1d0dc9affbf9c9b3bc333fa665f93b8fc35da770
Santos <44979635+skosantosh@users.noreply.github.com>
2021-10-26 17:42:29 -0500

    first commit

--- method_inheritance.py
+++ method_inheritance.py
@@ -8,17 +8,25 @@ class Rectagle():
     def draw(self):
         print('Drawing')
 
+
 # This class inheritance from Rectagle class
 # this doesn't vahe mothod whoIam, draw it stell inheritance from rectagle
 # if we un comment Rectagle.__init__(selt) it also inheritance this too.
-
-
 class square(Rectagle):
     def __init__(self):
         # Rectagle.__init__(self)
         print("Square Created")
 
+# add neww method
+    def color(self):
+        print('Red')
+
+# Orverride method from Rectagle Class
+    def draw(self):
+        print("Drawing done.")
+
 
 mySqu = square()
 mySqu.whoAmI()
 mySqu.draw()
+mySqu.color()
